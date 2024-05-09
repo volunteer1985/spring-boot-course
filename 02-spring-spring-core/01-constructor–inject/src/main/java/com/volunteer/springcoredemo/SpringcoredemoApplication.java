994966a0e5093f8f6077188ffc76bbc96a23bd43
Ivan-Bogdan Chornobai <ivanbogdanster@gmail.com>
2024-05-09 11:27:30 +0300

    finish component scan section

--- 02-spring-spring-core/01-constructor–inject/src/main/java/com/volunteer/springcoredemo/SpringcoredemoApplication.java
+++ 02-spring-spring-core/01-constructor–inject/src/main/java/com/volunteer/springcoredemo/SpringcoredemoApplication.java
@@ -3,6 +3,7 @@ package com.volunteer.springcoredemo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class SpringcoredemoApplication {
 
---
+++ 02-spring-spring-core/02-component-scanning/src/main/java/com/volunteer/springcoredemo/SpringcoredemoApplication.java
@@ -0,0 +1,16 @@
+package com.volunteer.springcoredemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+//@SpringBootApplication(scanBasePackages = {"com.volunteer.springcoredemo",
+//											"com.volunteer.util"})
+
+@SpringBootApplication
+public class SpringcoredemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringcoredemoApplication.class, args);
+	}
+
+}
