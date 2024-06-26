4b2ae9e6026b8ce15e47c00a744bb77bff45f6d6
Ivan Chornobai <ichornobai@yahoo.com>
2024-06-26 08:20:01 +0300

    Lazy initialization global property added

--- 02-spring-spring-core/06-lazy-initialization/src/main/java/com/volunteer/springcoredemo/common/TrackCoach.java
+++ 02-spring-spring-core/06-lazy-initialization/src/main/java/com/volunteer/springcoredemo/common/TrackCoach.java
@@ -1,5 +1,6 @@
 package com.volunteer.springcoredemo.common;
 
+import org.springframework.context.annotation.Lazy;
 import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
