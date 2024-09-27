f21b884a165f04965027ee52ae307ef0e985bbf0
Ivan-Bogdan Chornobai <ivanbogdanster@gmail.com>
2024-09-27 15:30:09 +0300

    CRUD implemented for Employee

--- 04-spring-boot-rest-crud/02-spring-boot-rest-crud-employee/src/main/java/com/someserv/cruddemo/rest/EmployeeRestController.java
+++ 04-spring-boot-rest-crud/02-spring-boot-rest-crud-employee/src/main/java/com/someserv/cruddemo/rest/EmployeeRestController.java
@@ -3,9 +3,8 @@ package com.someserv.cruddemo.rest;
 import com.someserv.cruddemo.dao.EmployeeDAO;
 import com.someserv.cruddemo.entity.Employee;
 import com.someserv.cruddemo.service.EmployeeService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -17,8 +16,46 @@ public class EmployeeRestController {
     EmployeeRestController(EmployeeService theEmployeeService){
         employeeService = theEmployeeService;
     }
+
     @GetMapping("/employees")
     public List<Employee> findAll(){
         return employeeService.findAll();
     }
+
+    @GetMapping("/employees/{employeeId}")
+    public Employee getEmployeeById(@PathVariable int employeeId){
+        Employee theEmployee = employeeService.findById(employeeId);
+        if(theEmployee == null){
+            throw new RuntimeException("Employee not found by id " + employeeId);
+        }
+       return theEmployee;
+    }
+
+    @PostMapping("/employees")
+    public Employee addEmployee(@RequestBody Employee theEmployee){
+        theEmployee.setId(0);
+        Employee dbEmployee = employeeService.save(theEmployee);
+        return dbEmployee;
+    }
+
+    @PutMapping("/employees")
+    public Employee updateEmployee(@RequestBody Employee theEmployee){
+        Employee dbEmployee = employeeService.save(theEmployee);
+        return dbEmployee;
+    }
+    
+    @DeleteMapping("/employees/{employeeId}")
+    public String deleteById(@PathVariable int employeeId) {
+    	Employee dbEmployee = employeeService.findById(employeeId);
+    	if (dbEmployee == null) {
+    		throw new RuntimeException("No employee found with id=" + employeeId);
+    	}
+    	employeeService.deleteById(employeeId);
+		return "Emloyee successfully deleted by id=" + employeeId;
+		
+    }
+    
+
+
+
 }
