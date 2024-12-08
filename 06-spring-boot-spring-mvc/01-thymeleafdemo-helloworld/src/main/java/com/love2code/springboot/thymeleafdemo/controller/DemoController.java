42068b0afa41b38f0d7a718a647b0226069e3a84
Ivan-Bogdan Chornobai <ivanbogdanster@gmail.com>
2024-12-09 00:44:13 +0200

    input form for student name created

---
+++ 06-spring-boot-spring-mvc/01-thymeleafdemo-helloworld/src/main/java/com/love2code/springboot/thymeleafdemo/controller/DemoController.java
@@ -0,0 +1,16 @@
+package com.love2code.springboot.thymeleafdemo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class DemoController {
+
+    @GetMapping("/hello")
+    public String sayHallo(Model theModel){
+        theModel.addAttribute("theDate", java.time.LocalDateTime.now());
+
+        return "helloworld";
+    }
+}
