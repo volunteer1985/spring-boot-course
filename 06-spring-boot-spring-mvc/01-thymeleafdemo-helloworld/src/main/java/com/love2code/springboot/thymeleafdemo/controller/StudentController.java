d0829560802c0e57797693a1f1812ed76ed2f799
Ivan-Bogdan Chornobai <ivanbogdanster@gmail.com>
2024-12-19 01:11:30 +0200

    drop list for countries using application.properties implemented

---
+++ 06-spring-boot-spring-mvc/01-thymeleafdemo-helloworld/src/main/java/com/love2code/springboot/thymeleafdemo/controller/StudentController.java
@@ -0,0 +1,44 @@
+package com.love2code.springboot.thymeleafdemo.controller;
+
+import com.love2code.springboot.thymeleafdemo.model.Student;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
+
+@Controller
+public class StudentController {
+
+    @Value("${countries}")
+    private List<String> countries;
+
+    @GetMapping("/showStudentForm")
+    public String showStudentForm(Model theModel){
+
+        //Create Student object
+        Student theStudent = new Student();
+
+        //add Student object to Model attributes
+        theModel.addAttribute("student", theStudent);
+
+        //add countries list to the model
+        theModel.addAttribute("countries", countries);
+
+        return "student-form";
+    }
+
+    @PostMapping("/processStudentForm")
+    public String processForm(@ModelAttribute("student") Student theStudent){
+
+        //logging student
+        System.out.println( "Student " + theStudent.getFirstName() + " " + theStudent.getLastName());
+        System.out.println("Country: " + theStudent.getCountry());
+
+        return "student-confirmation";
+    }
+
+}
