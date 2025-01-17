5d64e61e147bcd572f494c41233244816d9b0777
Ivan-Bogdan Chornobai <ivanbogdanster@gmail.com>
2025-01-17 14:19:50 +0200

    Last name validation with InitBinder implemented

---
+++ 06-spring-boot-spring-mvc/02-validationdemo/src/main/java/com/springdemo/mvc/CustomerController.java
@@ -0,0 +1,41 @@
+package com.springdemo.mvc;
+
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.Size;
+import org.springframework.beans.propertyeditors.StringTrimmerEditor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.InitBinder;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class CustomerController {
+
+    @InitBinder
+    public void initBinder(WebDataBinder webDataBinder){
+        StringTrimmerEditor stringTrimmerEditor = new StringTrimmerEditor(true);
+        webDataBinder.registerCustomEditor(String.class, stringTrimmerEditor);
+    }
+
+    @GetMapping("/")
+    public String showForm(Model theModel){
+        theModel.addAttribute("customer", new Customer());
+        return "customer-form";
+    }
+
+    @PostMapping("/processForm")
+    public String processForm(@Valid @ModelAttribute("customer") Customer theCustomer, BindingResult theBindingResult){
+
+        System.out.println("Customer last name: |" + theCustomer.getLastName() + "|");
+
+        if(theBindingResult.hasErrors()){
+            return "customer-form";
+        } else {
+            return "customer-confirmation";
+        }
+    }
+}
