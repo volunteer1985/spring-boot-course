ac18723af75901a9ef8ecb6ad39133b86335845b
Ivan-Bogdan Chornobai <ivanbogdanster@gmail.com>
2025-06-06 22:42:56 +0300

    Log out function implemented

---
+++ 08-spring-boot-spring-mvc-security/01-spring-boot-spring-mvc-security-default/src/main/java/com/ibogdan/springsecuritydemo/SpringmvcsecuritydemoApplication.java
@@ -0,0 +1,13 @@
+package com.ibogdan.springsecuritydemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringmvcsecuritydemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringmvcsecuritydemoApplication.class, args);
+	}
+
+}
---
+++ 08-spring-boot-spring-mvc-security/01-spring-boot-spring-mvc-security-default/src/test/java/com/ibogdan/springsecuritydemo/SpringmvcsecuritydemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ibogdan.springsecuritydemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringmvcsecuritydemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
