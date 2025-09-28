9a9685aae5438bcb036ce3902833ccf02fa34978
Ivan-Bogdan Chornobai <ivanbogdanster@gmail.com>
2025-09-28 23:39:26 +0300

    jdbc usage for authentication implemented

--- 08-spring-boot-spring-mvc-security/01-spring-boot-spring-mvc-security-default/src/main/java/com/ibogdan/springsecuritydemo/security/DemoSecurityConfiguration.java
+++ 08-spring-boot-spring-mvc-security/01-spring-boot-spring-mvc-security-default/src/main/java/com/ibogdan/springsecuritydemo/security/DemoSecurityConfiguration.java
@@ -1,16 +1,25 @@
 package com.ibogdan.springsecuritydemo.security;
 
-import jakarta.servlet.DispatcherType;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
 
 @Configuration
 public class DemoSecurityConfiguration {
+
+    // Set JDBC users support
+
+    @Bean
+    public UserDetailsManager userDetailsManager(DataSource dataSource){
+        return new JdbcUserDetailsManager(dataSource);
+    }
+
+   /*
     @Bean
     public InMemoryUserDetailsManager userDetailsManager() {
         UserDetails john = User.builder()
@@ -30,6 +39,7 @@ public class DemoSecurityConfiguration {
                 .build();
         return new InMemoryUserDetailsManager(john, mary, susan);
     }
+        */
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
